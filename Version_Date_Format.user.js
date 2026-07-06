2e543e2cc7841671ff6ef80041bd2753612d6b30
Personwritep <89448434+personwritep@users.noreply.github.com>
2026-07-07 08:04:54 +0900

    Update script name and version details

--- Version_Date_Format.user.js
+++ Version_Date_Format.user.js
@@ -1,15 +1,15 @@
 // ==UserScript==
-// @name        Version Date Format ⭐
+// @name        Version Date Update ⭐
 // @namespace        http://tampermonkey.net/
-// @version        0.1
-// @description        マニュアルの「バージョン」「更新日付」の書式変換ツール
+// @version        0.2
+// @description        記事中の「バージョン」「更新日付」を記入するツール　「Alt＋F9」
 // @author        Ameba Blog User
 // @match        https://blog.ameba.jp/ucs/entry/srventry*
 // @exclude        https://blog.ameba.jp/ucs/entry/srventrylist.do*
 // @icon        https://www.google.com/s2/favicons?sz=64&domain=ameblo.jp
 // @grant        none
-// @updateURL        https://github.com/personwritep/Version_Date_Update/raw/main/Version_Date_Format.user.js
-// @downloadURL        https://github.com/personwritep/Version_Date_Update/raw/main/Version_Date_Format.user.js
+// @updateURL        https://github.com/personwritep/Version_Date_Update/raw/main/Version_Date_Update.user.js
+// @downloadURL        https://github.com/personwritep/Version_Date_Update/raw/main/Version_Date_Update.user.js
 // ==/UserScript==
 
 
@@ -26,120 +26,174 @@ function wait_target(){
 
 
 
-function main(){
-    let origin; // 変換前のデータ
 
+function main(){
     let target=document.getElementById('cke_1_contents'); // 監視 target
-    let monitor=new MutationObserver(get_ver_date);
+    let monitor=new MutationObserver(catch_key);
     monitor.observe(target, {childList: true}); // ショートカット待受け開始
 
-    get_ver_date();
+    catch_key();
+
+    function catch_key(){
+        document.addEventListener("keydown", check_key); // iframe外のキー取得
+
+        let editor_iframe=document.querySelector('.cke_wysiwyg_frame');
+        if(editor_iframe){ // iframe読込みが実行条件
+            let iframe_doc=editor_iframe.contentWindow.document;
+            iframe_doc.addEventListener("keydown", check_key); } // iframe内のキー取得
+
+        function check_key(event){
+            if(event.keyCode==120 && event.altKey){ // 「Alt+F9」キー入力
+                event.preventDefault();
+                set_mark(); }
+        } // check_key
+
+    } // catch_key
 
 
-    function get_ver_date(){
+
+    function set_mark(){
+        let panel=
+            '<div id="update_panel">'+
+            'Ver No: <input class="ver_num" type="number" min="0.1" step="0.1">'+
+            '<input class="up_date" type="submit" value="Date Set">'+
+            '<input class="update_reset" type="submit" value="Reset">'+
+            '<input class="update_close" type="submit" value="✖">'+
+            '<style>'+
+            '#update_panel { position: fixed; top: 30vh; left: 20px; z-index: 100; '+
+            'padding: 20px; border: 1px solid #aaa; background: #fff; '+
+            'font: normal 16px/22px Meiryo; } '+
+            '.ver_num { width: 68px; padding: 2px 2px 0 0; text-align: center; } '+
+            'input[type=number].ver_num::-webkit-inner-spin-button { '+
+            'height: 20px; margin-top: 2px; } '+
+            '.up_date { padding: 2px 6px 0; margin: 0 20px; } '+
+            '.update_reset { padding: 2px 6px 0; margin: 0 10px; } '+
+            '.update_close { padding: 2px 6px 0; } '+
+            '</style></div>';
+
+        if(!document.querySelector('#update_panel')){
+            document.body.insertAdjacentHTML('beforeend', panel); }
+
+
+
         let editor_iframe=document.querySelector('.cke_wysiwyg_frame');
         if(editor_iframe){
             let iframe_doc=editor_iframe.contentWindow.document;
             if(iframe_doc){
 
-                iframe_doc.onclick=function(event){
-                    let elem=iframe_doc.elementFromPoint(event.clientX, event.clientY);
-                    if(elem){
-                        if(elem.tagName=="P"){
-                            origin=elem.outerHTML; // 変換前のデータ
-                            let last_ver='';
-                            let last_update='';
+                let origin0; // 処理前の対象行のデータ type0
+                let origin1; // 処理前の対象行のデータ type1
+                let ver_date_disp; // 処理対象行
 
-                            let ver_index=elem.textContent.indexOf('ver.');
-                            let ver_disp=elem.textContent.substr(ver_index, 10);
-                            if(ver_disp){
-                                last_ver=parseFloat(ver_disp.replace('ver.', '')).toFixed(1); }
+                let datedisp=iframe_doc.querySelector('.update_date'); // 更新対象日付のクラス名
+                if(datedisp){
+                    ver_date_disp=datedisp.parentElement;
+                    if(ver_date_disp.tagName=="P" || ver_date_disp.tagName=="DIV"){
+                        if(ver_date_disp.querySelector('.update_ver')){ // ver表示を持つ更新対象日付表示
+                            origin0=ver_date_disp.innerHTML; }
+                        else{
+                            origin0='';
+                            origin1=datedisp.innerHTML; }} // ver表示を伴わない更新対象日付表示
+                    else{
+                        origin0='';
+                        origin1=datedisp.innerHTML; } // ver表示を伴わない更新対象日付表示
 
-                            let year_disp=elem.textContent.match(/\b\d{4}\b/g);
-                            let date_index=elem.textContent.indexOf(year_disp);
-                            let date_disp=elem.textContent.substr(date_index, 10);
-                            if(date_disp){
-                                if(date_disp.includes('年')){ // 年月日表示
-                                    let year=date_disp.split('年')[0];
-                                    let month=date_disp.split('年')[1].split('月')[0];
-                                    let day=date_disp.split('月')[1].split('日')[0];
-                                    last_update=year +'.'+ month.padStart(2, '0') +'.'+ day.padStart(2, '0'); }
-                                else{ // ドット表示
-                                    let all=date_disp.split('.');
-                                    let year=all[0];
-                                    let month=all[1];
-                                    let day=all[2].replace(/[^0-9]/g, '');
-                                    last_update=year +'.'+ month.padStart(2, '0') +'.'+ day.padStart(2, '0'); }
-
-                                if(last_ver!='' && last_update!=''){ // 変換ツールの起動条件
-                                    disp_panel(elem, last_ver, last_update); }
-
-                            }}}
-
-                } // iframe_doc.onclick
-
-            }}}
+                    datedisp.scrollIntoView({ block: "center" }); } // if(datedisp)
+                else{ // 更新対象日付のクラス名を指定した日付表示が無い場合
+                    origin0=''; }
 
 
 
-    function disp_panel(elem, last_ver, last_update){
-        let panel=
-            '<div id="rewrite_panel">'+
-            '<input class="renew_text" type="text">'+
-            '<input class="rewrite" type="submit" value="Rewrite">'+
-            '<input class="reset" type="submit" value="Reset">'+
-            '<input class="close" type="submit" value="✖">'+
-            '<style>'+
-            '#rewrite_panel { position: fixed; top: 30vh; left: 20px; z-index: 100; '+
-            'padding: 20px; border: 1px solid #aaa; background: #fff; '+
-            'font: normal 16px/22px Meiryo; } '+
-            '.renew_text { width: 300px; padding: 2px 12px 0; } '+
-            '.rewrite { padding: 2px 6px 0; margin: 0 20px; } '+
-            '.reset { padding: 2px 6px 0; margin: 0 10px; } '+
-            '.close { padding: 2px 6px 0; } '+
-            '</style></div>';
+                let ver_num=document.querySelector('#update_panel .ver_num');
+                if(ver_num){
+                    if(origin0!=''){
+                        let update_ver=ver_date_disp.querySelector('.update_ver');
+                        let version=parseFloat(update_ver.textContent.replace('ver.', ''));
 
-        if(!document.querySelector('#rewrite_panel')){
-            document.body.insertAdjacentHTML('beforeend', panel); }
+                        if(version){ // 少数表示でないver表記は非対応
+                            ver_num.value=version.toFixed(1);
 
-        let renew_text=document.querySelector('#rewrite_panel .renew_text');
-        if(renew_text){
-            renew_text.value='ver.'+ last_ver +' 以降に対応　'+ last_update +' 更新'; }
+                            ver_num.onchange=function(){
+                                let new_ver=(ver_num.value/1).toFixed(1);
+                                ver_num.value=new_ver;
+                                update_ver.textContent='ver.'+ new_ver; }}}
+                    else{
+                        ver_num.disabled=true;
+                        ver_num.style.opacity='0.5'; }}
 
 
 
-        let rewrite=document.querySelector('#rewrite_panel .rewrite');
-        if(rewrite){
-            rewrite.onclick=()=>{
-                let new_data=
-                    '<p style="text-align: right; padding: .5em 1.5em 0;">'+
-                    '<span class="update_ver">ver.'+ last_ver +'</span> 以降に対応　'+
-                    '<span class="update_date">'+ last_update +'</span> 更新</p>';
+                let up_date=document.querySelector('#update_panel .up_date');
+                if(up_date){
+                    let datedisp=iframe_doc.querySelector('.update_date'); // 更新対象日付のクラス名
+                    if(datedisp){
+                        up_date.onclick=()=>{
+                            let date=new Date();
+                            let year=date.getFullYear();
+                            let month=date.getMonth()+1;
+                            let day=date.getDate();
 
-                elem.outerHTML=new_data; }}
+                            let d_str=datedisp.textContent;
+                            if(d_str.includes('年') || d_str.includes('月') || d_str.includes('日')){ // 年月日表示
+                                let index=d_str.search(/[0-9]/);
+                                let base_str=d_str.replace(/[0-9]|[年月日]/g, '');
+                                let top_str=base_str.slice(0, index);
+                                let end_str=base_str.slice(index);
+                                datedisp.textContent=
+                                    top_str+ year +'年'+ month +'月'+ day +'日'+ end_str; }
+
+                            else if(d_str.includes('.')){ // ドット繋ぎ表示
+                                let index=d_str.search(/[0-9]/);
+                                let base_str=d_str.replace(/[0-9]|\./g, '');
+                                let top_str=base_str.slice(0, index);
+                                let end_str=base_str.slice(index);
+                                datedisp.textContent=
+                                    top_str+
+                                    year +'.'+ String(month).padStart(2, '0') +'.'+ String(day).padStart(2, '0')+
+                                    end_str; }
+
+                        } // up_date.onclick
+
+                    } // if(datedisp)
+                    else{
+                        up_date.disabled=true;
+                        up_date.style.opacity='0.5'; }
+
+                } // if(up_date)
 
 
-        let reset=document.querySelector('#rewrite_panel .reset');
-        if(reset){
-            reset.onclick=()=>{
-                let editor_iframe=document.querySelector('.cke_wysiwyg_frame');
-                if(editor_iframe){
-                    let iframe_doc=editor_iframe.contentWindow.document;
-                    if(iframe_doc){
-                        let date_span=iframe_doc.querySelector('.update_date');
-                        if(date_span){
-                            let new_elem=date_span.closest('p');
-                            if(new_elem){
-                                new_elem.outerHTML=origin; }}}}}}
+
+                let update_reset=document.querySelector('#update_panel .update_reset');
+                if(update_reset){
+                    update_reset.onclick=()=>{
+                        let datedisp=iframe_doc.querySelector('.update_date'); // 更新対象日付のクラス名
+                        if(datedisp){
+                            if(origin0!=''){ // ver表示を持つ更新対象日付表示
+                                ver_date_disp=datedisp.parentElement;
+                                if(ver_date_disp.tagName=="P" || ver_date_disp.tagName=="DIV"){
+                                    ver_date_disp.innerHTML=origin0; }}
+                            else{ // ver表示を伴わない更新対象日付表示
+                                datedisp.innerHTML=origin1; }
+
+                        } // if(datedisp)
+
+                        set_mark(); // スクリプトの再起動
+
+                    } // update_reset.onclick
+
+                } // if(update_reset)
 
 
 
-        let close=document.querySelector('#rewrite_panel .close');
-        if(close){
-            close.onclick=()=>{
-                if(document.querySelector('#rewrite_panel')){
-                    document.querySelector('#rewrite_panel').remove(); }}}
+                let update_close=document.querySelector('#update_panel .update_close');
+                if(update_close){
+                    update_close.onclick=()=>{
+                        if(document.querySelector('#update_panel')){
+                            document.querySelector('#update_panel').remove(); }}}
 
-    } // disp_panel
+
+            }} // if(editor_iframe)
+
+    } // set_mark()
 
 } // main()
